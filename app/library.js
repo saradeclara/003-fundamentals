c986c5ad5ee2304e10f7f158cdd435d488f7da89
James Heggs <james.heggs@closconsultancy.com>
2018-10-25 15:46:15 +0100

    Implement functions for tasks

--- app/library.js
+++ app/library.js
@@ -2,29 +2,45 @@ const books = [
     {
         title: "Don't Make Me Think",
         author: "Steve Krug",
-        available: true
+        available: true,
+        isbn: 123
     },
     {
         title: "Site Reliability Engineering",
         author: "Niall Richard Murphy",
-        available: true
+        available: true,
+        isbn: 456
     },
     {
         title: "Fantastic Mr Fox",
         author: "Roald Dahl",
-        available: true
+        available: true,
+        isbn: 789
     }
 ];
 
 function getAllBooks() {
-    
     return books;
 }
 
+function checkoutBook(isbnNumber) {
+    let matchingBooks = books.filter(function(book) {
+        return book.isbn == isbnNumber;
+    })
+
+    if(matchingBooks != null && matchingBooks.length > 0 ) {
+        matchingBooks[0].available = false;
+    }
+}
+
 function getAllAvailableBooks() {
+    return books.filter(function(book) {
+        return book.available;
+    });
 }
 
 module.exports = {
     getAllBooks,
-    getAllAvailableBooks
+    getAllAvailableBooks,
+    checkoutBook
 };
--- test/library.test.js
+++ test/library.test.js
@@ -2,15 +2,70 @@ const library = require('../app/library');
 
 test('Check we have all books and stocked correctly', () => {
   const expectedBooksInLibrary = [{
-
+    title: "Don't Make Me Think",
+    author: "Steve Krug",
+    available: true,
+    isbn: 123
+  },
+  {
+    title: "Site Reliability Engineering",
+    author: "Niall Richard Murphy",
+    available: true,
+    isbn: 456
+  },
+  {
+    title: "Fantastic Mr Fox",
+    author: "Roald Dahl",
+    available: true,
+    isbn: 789
   }];
 
   expect(library.getAllBooks()).toEqual(expectedBooksInLibrary);
 });
 
-test('Check available books', () => {
-  const availableBooksInLibrary = [{
+test('Checkout a book', () => {
+  let expectedBooksInLibrary = [{
+    title: "Don't Make Me Think",
+    author: "Steve Krug",
+    available: true,
+    isbn: 123
+  },
+  {
+    title: "Site Reliability Engineering",
+    author: "Niall Richard Murphy",
+    available: true,
+    isbn: 456
+  },
+  {
+    title: "Fantastic Mr Fox",
+    author: "Roald Dahl",
+    available: true,
+    isbn: 789
+  }];
 
+  // Assert that all books are available
+  expect(library.getAllBooks()).toEqual(expectedBooksInLibrary);
+
+  // Then checkout a book so we change the status of our test data
+  expectedBooksInLibrary[1].available = false;
+
+  // And then we checkout the book and check that our test data matches what we expect
+  library.checkoutBook(456);
+  expect(library.getAllBooks()).toEqual(expectedBooksInLibrary);
+});
+
+test('Check available books', () => {
+  let availableBooksInLibrary = [{
+    title: "Don't Make Me Think",
+    author: "Steve Krug",
+    available: true,
+    isbn: 123
+  },
+  {
+    title: "Fantastic Mr Fox",
+    author: "Roald Dahl",
+    available: true,
+    isbn: 789
   }];
 
   expect(library.getAllAvailableBooks()).toEqual(availableBooksInLibrary);
