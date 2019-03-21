4707f66736a2ca60aff6393087f1ad8de59cdd0c
Sara De Clara <saradeclara@gmail.com>
2019-03-21 18:18:27 +0000

    Added new test to check that a book can be added to library

--- test/library.test.js
+++ test/library.test.js
@@ -4,6 +4,7 @@ const Book = require("../app/book");
 const book1 = new Book('Don\'t Make Me Think', 'Steve Krug', 123);
 const book2 = new Book('Site Reliability Engineering', 'Niall Richard Murphy', 456);
 const book3 = new Book('Fantastic Mr Fox', 'Roald Dahl', 789);
+const book4 = new Book('Growth Mindset', 'Carol Dweck', 992);
 
 test("Check that a library can be instantiated with some books", () => {
   const library = new Library([
@@ -15,7 +16,11 @@ test("Check that a library can be instantiated with some books", () => {
 });
 
 test("Check that a book can be added to the library", () => {
-  const library = new Library([]);
-  library.add();
-  expect(library.books).toEqual([]);
+  const library = new Library([
+    book1, book2, book3
+  ]);
+  library.add(book4);
+  expect(library.books).toEqual([
+    book1, book2, book3, book4
+  ]);
 });
